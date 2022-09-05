c3c48b324e7a46c7aaed3a1d93da460cc14db34c
Rupali Harjai <rharjai@beesolvertechnology.com>
2022-09-05 17:23:11 +0530

    add css for responsive badge

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -10,8 +10,11 @@ export class NewsItem extends Component {
     return (
       <div className='my-4'>
         <div className ="card" >
-        <div>
-        <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" style={{left:'85%', zIndex:'1'}}>
+        <div style={{display:'flex',
+        justifyContent:'flex-end',
+        position: 'absolute',
+        right: 0}}>
+        <span className=" badge rounded-pill bg-danger" >
     {source} 
   </span>
         </div>
