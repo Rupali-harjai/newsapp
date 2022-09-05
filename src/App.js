ccfc7a6fc6ecbd0ec3ea2555e508003b735e6a1d
Rupali Harjai <rharjai@beesolvertechnology.com>
2022-09-05 13:31:23 +0530

    top loading bar

--- src/App.js
+++ src/App.js
@@ -14,33 +14,40 @@ import {
 export default class App extends Component {
 
   pageSize = 10;
+  state = {
+    progress: 0
+  }
+setProgress =(progress)=>{
+  this.setState({progress: progress})
+}
   render() {
     return (
       <div>
          <Router>
          <LoadingBar
         color='#f11946'
-        progress={10}
+        height={3}
+        progress={this.state.progress}
        
       />
        <Navbar/>
       
        <Routes>
-          <Route exact path="/" element={<News key="1" pageSize ={this.pageSize} country="in" category ="general"/>}/>
+          <Route exact path="/" element={<News setProgress = {this.setProgress}  key="1" pageSize ={this.pageSize} country="in" category ="general"/>}/>
 
-          <Route exact path="/business" element={<News key="2" pageSize ={this.pageSize} country="in" category ="business"/>}/>
+          <Route exact path="/business" element={<News setProgress = {this.setProgress}  key="2" pageSize ={this.pageSize} country="in" category ="business"/>}/>
 
-           <Route exact path="/entertainment" element={<News key="3" pageSize ={this.pageSize} country="in" category ="entertainment"/>}/>
+           <Route exact path="/entertainment" element={<News setProgress = {this.setProgress}  key="3" pageSize ={this.pageSize} country="in" category ="entertainment"/>}/>
 
-           <Route exact path="/general" element={<News key="4" pageSize ={this.pageSize} country="in" category ="general"/>}/>
+           <Route exact path="/general" element={<News setProgress = {this.setProgress}  key="4" pageSize ={this.pageSize} country="in" category ="general"/>}/>
 
-           <Route exact path="/health" element={<News key="5" pageSize ={this.pageSize} country="in" category ="health"/>}/>
+           <Route exact path="/health" element={<News setProgress = {this.setProgress}  key="5" pageSize ={this.pageSize} country="in" category ="health"/>}/>
 
-           <Route exact path="/science" element={<News key="6" pageSize ={this.pageSize} country="in" category ="science"/>}/>
+           <Route exact path="/science" element={<News setProgress = {this.setProgress}  key="6" pageSize ={this.pageSize} country="in" category ="science"/>}/>
 
-           <Route exact path="/sports" element={<News key="7" pageSize ={this.pageSize} country="in" category ="sports"/>}/>
+           <Route exact path="/sports" element={<News setProgress = {this.setProgress}  key="7" pageSize ={this.pageSize} country="in" category ="sports"/>}/>
 
-           <Route exact path="/technology" element={<News key="8" pageSize ={this.pageSize} country="in" category ="technology"/>}/>
+           <Route exact path="/technology" element={<News setProgress = {this.setProgress}  key="8" pageSize ={this.pageSize} country="in" category ="technology"/>}/>
         </Routes>
        </Router>
       </div>
