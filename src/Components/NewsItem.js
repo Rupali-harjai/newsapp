1dd68f03c86b858362bb6988d4b8656495d34402
Rupali Harjai <rharjai@beesolvertechnology.com>
2022-09-07 12:22:12 +0530

    change class to function based

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,12 +1,8 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class NewsItem extends Component {
+const NewsItem = (props) => {
 
-
-
-
-  render() {
-    let {title, description, imageUrl, newsUrl, author, date,source } = this.props;
+    let {title, description, imageUrl, newsUrl, author, date,source } = props;
     return (
       <div className='my-4'>
         <div className ="card" >
@@ -30,6 +26,6 @@ export class NewsItem extends Component {
       </div>
     )
   }
-}
+
 
 export default NewsItem
